06277ab68ab5f45c79d0067a7b562629a5a3d813
Gilad Kutiel <gilad.kutiel@gmail.com>
2026-08-29 23:48:27 +0300

    fix

--- model.py
+++ model.py
@@ -22,6 +22,7 @@ class Model(nn.Module):
                 kernel_size=3,
                 stride=1,
                 padding=0,
+                bias=False
             ),
 
             nn.ReLU(),
@@ -38,6 +39,7 @@ class Model(nn.Module):
                         kernel_size=3,
                         stride=1,
                         padding=0,
+                        bias=False
                     ),
 
                     nn.ReLU(),
