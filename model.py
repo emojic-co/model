b59588c99cbc85fa96933a7b58606afef53c2a2d
Gilad Kutiel <gilad.kutiel@gmail.com>
2026-08-29 17:19:30 +0300

    fix

--- model.py
+++ model.py
@@ -27,7 +27,7 @@ class Model(nn.Module):
             ),
             nn.ReLU(),
 
-            nn.AvgPool1d(
+            nn.MaxPool1d(
                 kernel_size=3,
                 stride=2),
         )
