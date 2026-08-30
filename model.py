9cd9263a6f12169b33416f8bb35f3d41cec9faf1
Gilad Kutiel <gilad.kutiel@gmail.com>
2026-08-30 15:28:33 +0300

    fix

--- model.py
+++ model.py
@@ -7,6 +7,7 @@ from config import (
     CHAR_EMBED_SIZE,
     EMOJI_EMBED_SIZE,
     KERNEL,
+    MAX_POOL_SIZE,
 )
 from data import EMOJIS, FEELING, VOCAB_SIZE
 
@@ -29,7 +30,9 @@ class Layer(nn.Sequential):
 
             nn.BatchNorm1d(out_channels),
             nn.LeakyReLU(negative_slope=0.1),
-            nn.MaxPool1d(kernel_size=2, stride=2),
+            nn.MaxPool1d(
+                kernel_size=MAX_POOL_SIZE,
+                stride=MAX_POOL_SIZE),
         )
 
 
