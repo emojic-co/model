b3ec13f43831b138d915be58a83249c5a12d51ff
Gilad Kutiel <gilad.kutiel@gmail.com>
2026-08-30 08:59:16 +0300

    fix

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ MAX_TEXT_LEN = 42
 # not a slice of data.jsonl -- there is no eval-size knob here any more.
 
 # MODEL
-CHAR_EMBED_SIZE = 10
+CHAR_EMBED_SIZE = 16
 KERNELS = (3,)
 CHANNELS = (128,)
 
