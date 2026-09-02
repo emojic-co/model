300a42cfd8bb1b457614dbfbc7be6ba904d2f709
Gilad Kutiel <gilad.kutiel@gmail.com>
2026-09-02 23:54:29 +0300

    fix

--- model.py
+++ model.py
@@ -33,11 +33,8 @@ class TextEncoder(nn.Module):
 
         def conv_norm_relu(*, k: int, i: int, o: int) -> nn.Sequential:
             return nn.Sequential(
-                sn(nn.Conv1d(i, o,
-                             kernel_size=k,
-                             padding=k // 2,
-                             #  bias=False
-                             )),
+                sn(nn.Conv1d(i, o, kernel_size=k, padding=k // 2, bias=False)),
+                nn.BatchNorm1d(o),
                 nn.LeakyReLU(negative_slope=RELU_SLOPE))
 
         cs = ENCODER_CHANNELS
