46771b9b1e38ef850203931bba97a3a562964a33
Gilad Kutiel <gilad.kutiel@gmail.com>
2026-09-06 15:40:17 +0300

    train local

--- model/config.py
+++ model/config.py
@@ -26,8 +26,8 @@ assert ENCODER_KERNEL_SIZE % 2 == 1, \
     "encoder kernel size must be odd"
 
 # ENCODER_CHANNELS = [256, 128, 64]
-ENCODER_CHANNELS = [256, 128]
-ENCODER_DILATION = [1, 2]
+ENCODER_CHANNELS = [64, 128, 256]
+ENCODER_DILATION = [1, 2, 4]
 
 assert len(ENCODER_CHANNELS) == len(ENCODER_DILATION), \
     "encoder channels and dilation must have the same length"
