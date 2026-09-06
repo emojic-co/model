095332b15904d1f3f75f61fb95955091e4202c47
Gilad Kutiel <gilad.kutiel@gmail.com>
2026-09-06 15:29:22 +0300

    train local

--- model/config.py
+++ model/config.py
@@ -40,7 +40,7 @@ enc_str = " ".join([
         ENCODER_DILATION)])
 
 # EMOJI
-EMOJI_EMBED_SIZE = 42
+EMOJI_EMBED_SIZE = 64
 DROPOUT_EMOJI = 0.1
 
 emj_str = " ".join([str(p) for p in (EMOJI_EMBED_SIZE, DROPOUT_EMOJI)])
