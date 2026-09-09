60bb3093d742129599a0d793eb75475557a22dd8
Gilad Kutiel <gilad.kutiel@gmail.com>
2026-09-09 09:33:00 +0300

    fix

--- tools/data/regen.ts
+++ tools/data/regen.ts
@@ -4,8 +4,8 @@ import { splitEmojis } from "./emoji.ts"
 import { normalize } from "./normalize.ts"
 import { STYLE_SET } from "./styles.ts"
 
-const MIN_COUNT = 100
-const MAX_COUNT = 20000
+const MIN_COUNT = 50
+const MAX_COUNT = 1000
 const EVAL_SIZE = 2000
 
 const MIN_MAX_RATIO = 10
