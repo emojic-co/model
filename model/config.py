358d1cf38534aa99111ff24eae0da69444805bfc
Gilad Kutiel <gilad.kutiel@gmail.com>
2026-09-17 18:28:49 +0300

    train

--- model/config.py
+++ model/config.py
@@ -35,7 +35,7 @@ ENCODER_KERNEL_SIZE = 3
 assert ENCODER_KERNEL_SIZE % 2 == 1, \
     "encoder kernel size must be odd"
 
-ENCODER_CHANNELS = [100, 100, 100, 100]
+ENCODER_CHANNELS = [90, 130, 100, 60]
 ENCODER_DILATION = [1, 2, 4, 8]
 
 # EMBEDDING
