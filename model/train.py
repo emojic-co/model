c162d29e964bd00a3ca4a09678ab09019702e8bb
Gilad Kutiel <gilad.kutiel@gmail.com>
2026-09-18 13:18:24 +0300

    train gan

--- model/train.py
+++ model/train.py
@@ -690,6 +690,7 @@ CODE_FILES = [
     f"{MODEL_DIR}/color.py",
     f"{MODEL_DIR}/config.py",
     f"{MODEL_DIR}/data.py",
+    f"{MODEL_DIR}/metric.py",
     f"{MODEL_DIR}/metrics.py",
     f"{MODEL_DIR}/model.py",
     f"{MODEL_DIR}/train.py",
