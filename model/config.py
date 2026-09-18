5513c8ec7b5a1e6257d7f7efe1a67a451046cd6b
Gilad Kutiel <gilad.kutiel@gmail.com>
2026-09-18 15:55:43 +0300

    train gan

--- model/config.py
+++ model/config.py
@@ -129,7 +129,7 @@ SAMPLING_SOURCES: dict[Source, SamplingSource] = {
     Source.COLOR: SamplingSource(
         COLOR_TERMS_JSONL,
         named_metric(Source.COLOR, Metric.R2, Split.VAL),
-        0, 0.5,
+        0, 0.2,
     ),
 }
 
