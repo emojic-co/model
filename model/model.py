295878287bc661328a0ba5cd717b2c9234cd7e58
Gilad Kutiel <gilad.kutiel@gmail.com>
2026-09-19 15:28:00 +0300

    train

--- model/model.py
+++ model/model.py
@@ -136,6 +136,7 @@ class ColorGen(nn.Module):
         self.net = nn.Sequential(
             *blk(Z_DIM + EMBED_SIZE_TEXT, GEN_HIDDEN_SIZE),
             *blk(GEN_HIDDEN_SIZE, GEN_HIDDEN_SIZE),
+            *blk(GEN_HIDDEN_SIZE, GEN_HIDDEN_SIZE),
             nn.Linear(GEN_HIDDEN_SIZE, COLOR_DIM))
 
     def forward(
