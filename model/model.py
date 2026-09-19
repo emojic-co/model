7ac8ae0a5b8cfdcbc958ced0461494b1f3b82c57
Gilad Kutiel <gilad.kutiel@gmail.com>
2026-09-19 10:40:37 +0300

    train

--- model/model.py
+++ model/model.py
@@ -134,7 +134,8 @@ class ColorGen(nn.Module):
         super().__init__()
 
         self.net = nn.Sequential(
-            *blk(EMBED_SIZE_TEXT + Z_DIM, GEN_HIDDEN_SIZE),
+            *blk(Z_DIM, GEN_HIDDEN_SIZE),
+            # *blk(EMBED_SIZE_TEXT + Z_DIM, GEN_HIDDEN_SIZE),
             *blk(GEN_HIDDEN_SIZE, GEN_HIDDEN_SIZE),
             nn.Linear(GEN_HIDDEN_SIZE, COLOR_DIM))
 
@@ -147,10 +148,11 @@ class ColorGen(nn.Module):
             z = torch.randn(
                 cond.shape[0], Z_DIM, device=cond.device, dtype=cond.dtype)
 
-        z = normalize(z, dim=-1)
+        # z = normalize(z, dim=-1)
         cond = normalize(cond, dim=-1)
 
-        seed = torch.cat([cond, z], dim=-1)
+        # seed = torch.cat([cond, z], dim=-1)
+        seed = z
 
         colors = self.net(seed)
         return tanh(colors) * COLOR_SHIFT
