ff31b377f944348d805e88b192504eba6be50b4a
Gilad Kutiel <gilad.kutiel@gmail.com>
2026-09-25 02:13:34 +0300

    fix

--- model/model.py
+++ model/model.py
@@ -162,7 +162,6 @@ class ColorEmbedding(nn.Module):
 
         self.net = nn.Sequential(
             *cblk(COLOR_DIM, EMBED_SIZE_COLOR),
-            # *cblk(EMBED_SIZE_COLOR, EMBED_SIZE_COLOR),
             *cblk(EMBED_SIZE_COLOR, EMBED_SIZE_COLOR))
 
     def forward(self, colors: torch.Tensor) -> torch.Tensor:
